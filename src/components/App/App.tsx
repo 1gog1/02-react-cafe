79983a8c4b35a0cf54d5ca17492f0d57db41abf9
Yehor Bychuk <110350969+1gog1@users.noreply.github.com>
2025-06-29 23:29:03 +0100

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.module.css'
+import { css } from 'styled-components'
+
+export default function App() {
+
+
+  return (
+    <>
+   <div className={css.app}></div></>
+  )
+}
+
+
