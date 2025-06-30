5fbdedbe79f4a3d9f86f14f971197d106e0fe9b5
Yehor Bychuk <110350969+1gog1@users.noreply.github.com>
2025-06-30 17:30:31 +0100

    update code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,58 @@
-import { useState } from 'react'
-import './App.module.css'
-import { css } from 'styled-components'
+import { useState } from "react";
+import css from "./App.module.css";
+import Notification from "../Notification/Notification";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import type { Votes, VoteType } from "../../types/votes";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import "modern-normalize/modern-normalize.css";
 
 export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
+  const handleVote = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    });
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const isReset = totalVotes !== 0;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
 
   return (
     <>
-   <div className={css.app}></div></>
-  )
+      <div className={css.app}></div>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={isReset}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
 }
-
-
--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,1 +1,8 @@
-import './Notification.module.css'
+import css from './Notification.module.css'
+
+
+export default function Notification() {
+    return (
+        <p className={css.message}>No feedback yet</p>
+    )
+}
